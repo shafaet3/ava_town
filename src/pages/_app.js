c86f50b25c6d41b93119c8a787b96f81cd4c7bf6
shafaet3 <shafaet3@gmail.com>
2023-05-02 00:46:57 +0600

    bootstrap js add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,17 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Head from "next/head";
 import "../styles/globals.css";
+import { useEffect } from "react";
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
