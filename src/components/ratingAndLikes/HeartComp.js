f9b77bfc814b8eb3d1b407f6c017881824d80294
shafaet3 <shafaet3@gmail.com>
2023-05-08 02:42:29 +0600

    rating and likes component

---
+++ src/components/ratingAndLikes/HeartComp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./heartComp.module.css";
+function HeartComp({ uniqueId }) {
+  return (
+    <div className={styles.customCheckbox}>
+      <input type="checkbox" id={`my-checkbox-${uniqueId}`} />
+      <label htmlFor={`my-checkbox-${uniqueId}`}></label>
+    </div>
+  );
+}
+
+export default HeartComp;
---
+++ src/components/ratingAndLikes/RatingComp.js
@@ -0,0 +1,48 @@
+import Rating from "react-rating";
+import { useState } from "react";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./ratingComp.module.css";
+function RatingComp({ data }) {
+  const [rating, setRating] = useState(0);
+
+  return (
+    <div>
+      <span className={styles.ratingsAndLikesText}>
+        <Rating
+          onClick={(value) => setRating(value)}
+          placeholderRating={rating}
+          initialRating={rating}
+          emptySymbol={
+            <FontAwesomeIcon
+              icon={faStar}
+              style={{
+                color: "#80808069",
+              }}
+            />
+          }
+          placeholderSymbol={
+            <FontAwesomeIcon
+              icon={faStar}
+              style={{
+                color: "#F9AE3F",
+              }}
+            />
+          }
+          fullSymbol={
+            <FontAwesomeIcon
+              icon={faStar}
+              style={{
+                color: "#F9AE3F",
+              }}
+            />
+          }
+        />{" "}
+        {rating}& {data.likes}
+        Likes
+      </span>
+    </div>
+  );
+}
+
+export default RatingComp;
