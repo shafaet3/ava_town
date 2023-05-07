f82badb360241c28b2bdd306a9ddce1e9b833b13
shafaet3 <shafaet3@gmail.com>
2023-05-08 02:41:17 +0600

    price component

---
+++ src/components/price/PriceComp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./priceComp.module.css";
+
+function PriceComp({ data }) {
+  return (
+    <span className={styles.avatarPrice}>
+      <span className={styles.avatarPriceIcon}>$</span> {data.price}
+    </span>
+  );
+}
+
+export default PriceComp;
