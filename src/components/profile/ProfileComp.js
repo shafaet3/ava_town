c457a9bd97f32c42183fa4677dd58353e205d14d
shafaet3 <shafaet3@gmail.com>
2023-05-08 02:42:00 +0600

    profile component

---
+++ src/components/profile/ProfileComp.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./profileComp.module.css";
+function ProfileComp({ data }) {
+  return (
+    <div>
+      <Image
+        src={data.imagePath}
+        alt={data.imagePath}
+        className={styles.avatarImgCircle}
+        width={21}
+        height={21}
+        priority={false}
+      />
+      <span className={styles.avatarName}>{data.name}</span>
+    </div>
+  );
+}
+
+export default ProfileComp;
