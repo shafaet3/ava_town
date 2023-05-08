4e22bd9f5bf65cc03788c8222948a1d9d3bf9e96
shafaet3 <shafaet3@gmail.com>
2023-05-08 23:23:43 +0600

    left sidebar done

--- src/components/LeftSidebar/Sidebar.js
+++ src/components/LeftSidebar/Sidebar.js
@@ -1,18 +1,17 @@
-import React from "react";
-import styles from "./sidebar.module.css";
 import Image from "next/image";
+import styles from "./sidebar.module.css";
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       {/* category start */}
       <div className={styles.sidebarContent}>
-        <label>
+        <label className={styles.label}>
           <span className={styles.sidebarTitle}>Category</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <span className={styles.sidebarText}>Full avatar</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <span className={styles.sidebarText}>Others</span>
         </label>
       </div>
@@ -20,10 +19,10 @@ function Sidebar() {
 
       {/* content start */}
       <div className={styles.sidebarContent}>
-        <label>
+        <label className={styles.label}>
           <span className={styles.sidebarTitle}>Contents</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>
             VRChat(Quest)
@@ -36,7 +35,7 @@ function Sidebar() {
             />
           </span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>
             VRChat(PCVR)
@@ -49,7 +48,7 @@ function Sidebar() {
             />
           </span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>Others</span>
         </label>
@@ -58,34 +57,34 @@ function Sidebar() {
 
       {/* price start */}
       <div className={styles.sidebarContent}>
-        <label>
+        <label className={styles.label}>
           <span className={styles.sidebarTitle}>Price</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>Under $10</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>$10 to $20</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>$20 to $30</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>$30 to $40</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>$40 to $50</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>$50 to $70</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>$70 & above</span>
         </label>
@@ -94,34 +93,34 @@ function Sidebar() {
 
       {/* Polygon amount start */}
       <div className={styles.sidebarContent}>
-        <label>
+        <label className={styles.label}>
           <span className={styles.sidebarTitle}>Polygon amount</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>Under △7,500</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>△7,500 to △10,000</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>△10,000 to △15,000</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>△15,000 to △20,000</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>△20,000 to △32,000</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>△32,000 to △70,000</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>△70,000 & Above</span>
         </label>
@@ -130,14 +129,14 @@ function Sidebar() {
 
       {/* Auto upload support start */}
       <div className={styles.sidebarContent}>
-        <label>
+        <label className={styles.label}>
           <span className={styles.sidebarTitle}>Auto upload support</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>Supported</span>
         </label>
-        <label>
+        <label className={styles.label}>
           <input type="checkbox" className={styles.inputCheckBox}></input>
           <span className={styles.sidebarInputText}>Unsupported</span>
         </label>
